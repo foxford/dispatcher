98b799bf5ba50a528436ae7d9bd851f944ee841b
Shamir Khodzha <khodzha.sh@gmail.com>
2021-04-26 19:21:42 +0300

    return minigroup host in api, fixed uniq recording index

--- migrations/20210421123120_AddDeletedAtToRecording.sql
+++ migrations/20210421123120_AddDeletedAtToRecording.sql
@@ -1,4 +1,2 @@
 -- Add migration script here
 ALTER TABLE recording ADD COLUMN deleted_at TIMESTAMPTZ;
-
-CREATE UNIQUE INDEX uniq_recording_per_class ON recording (class_id) WHERE deleted_at IS NULL;
