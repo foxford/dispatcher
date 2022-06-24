b4cbb25cfc88cf11b296aaae750b525c5940d33f
Dmitry Shlagoff <richyannet@yandex.ru>
2022-06-24 13:12:41 +0200

    Fix recordings URL

--- src/app/api/v1/webinar/download.rs
+++ src/app/api/v1/webinar/download.rs
@@ -57,11 +57,84 @@ pub async fn download(
 
 fn format_url(config: &StorageConfig, webinar: &Class, recording: &Recording) -> String {
     let mut url = config.base_url.clone();
-    url.set_path(&format!(
-        "/api/v2/backends/yandex/sets/ms.webinar.{}::{}/objects/mp4",
-        webinar.audience(),
-        recording.rtc_id()
-    ));
+    let recording_id = format!("ms.webinar.{}::{}", webinar.audience(), recording.rtc_id());
+    url.path_segments_mut()
+        .expect("cannot-be-a-base URL")
+        .extend(&[
+            "api",
+            "v2",
+            "backends",
+            "yandex",
+            "sets",
+            &recording_id,
+            "objects",
+            "mp4",
+        ]);
 
     url.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_helpers::prelude::*;
+
+    use chrono::{Duration, Utc};
+    use hyper::body::to_bytes;
+
+    #[tokio::test]
+    async fn create_webinar_timestamp() {
+        let agent = TestAgent::new("web", "user1", USR_AUDIENCE);
+
+        let db_pool = TestDb::new().await;
+
+        let webinar = {
+            let mut conn = db_pool.get_conn().await;
+            let webinar = factory::Webinar::new(
+                random_string(),
+                USR_AUDIENCE.to_string(),
+                (
+                    Bound::Unbounded,
+                    Bound::Excluded(Utc::now() - Duration::seconds(10)),
+                )
+                    .into(),
+                Uuid::new_v4(),
+                Uuid::new_v4(),
+            )
+            .insert(&mut conn)
+            .await;
+
+            factory::Recording::new(webinar.id(), Uuid::new_v4(), agent.agent_id().clone())
+                .transcoded_at(Utc::now())
+                .insert(&mut conn)
+                .await;
+
+            webinar
+        };
+
+        let mut authz = TestAuthz::new();
+        authz.allow(
+            agent.account_id(),
+            vec!["classrooms", &webinar.id().to_string()],
+            "download",
+        );
+
+        let state = TestState::new_with_pool(db_pool, authz);
+        let state = Arc::new(state);
+
+        let r = download(
+            Extension(state.clone()),
+            Path(webinar.id()),
+            AuthnExtractor(agent.agent_id().to_owned()),
+        )
+        .await
+        .expect("shouldn't fail");
+
+        let body = to_bytes(r.into_body()).await.unwrap();
+        let body: serde_json::Value = serde_json::from_slice(&body).unwrap();
+        assert!(body["url"]
+            .as_str()
+            .unwrap()
+            .starts_with(state.config().storage.base_url.as_str()));
+    }
 }
